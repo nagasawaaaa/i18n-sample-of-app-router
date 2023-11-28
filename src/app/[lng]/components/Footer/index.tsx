d2d4d356b7dd60371e688402c978dbb806dd292b
Satoshi Nagasawa <nagasawa@ham.works>
2023-11-28 10:29:12 +0900

    fix: 不要な代入の削除

--- src/app/[lng]/components/Footer/index.tsx
+++ src/app/[lng]/components/Footer/index.tsx
@@ -9,7 +9,7 @@ interface Props {
   path?: string;
 }
 export const Footer = async ({ lng, path = "" }: Props) => {
-  const { t, i18n } = await useTranslation(lng, "footer");
+  const { i18n } = await useTranslation(lng, "footer");
   // FIXME: i18next のバグっぽいのでとりあえずキャストする
   // Refs: https://github.com/i18next/react-i18next/issues/1693
   return <FooterBase i18n={i18n as i18N} lng={lng} path={path} />;
